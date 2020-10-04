6ffffa64cc9e2bcb86044d905a01f363c0526b13
Your Name <you@example.com>
2020-10-04 15:16:56 +0800

    'coderso'

--- src/network/request.js
+++ src/network/request.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export function request(config) {
   const instance = axios.create({
-    baseURL: 'http://152.136.185.210:8000/api/w6',
+    baseURL: '123456789',
     timeout: 5000
   })
   //axios拦截
